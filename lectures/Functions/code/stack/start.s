e257058282042975b816a14de58eed2b0c36bb9b
Julie Zelenski <zelenski@cs.stanford.edu>
2019-01-30 05:36:59 +0000

    Merge branch 'master' of https://github.com/cs107e/staff commit 32bfa81d0ce5060de848b4a9d83f37a5505a1b33 Merge: d2c5e37 5b5e036 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Tue Jan 29 21:31:50 2019 -0800
        Merge branch 'master' of https://github.com/cs107e/staff

--- lectures/Functions/code/stack/start.s
+++ lectures/Functions/code/stack/start.s
@@ -1,6 +1,6 @@
-.global _start
+.globl _start
 _start:
-    mov sp, #0x8000000  // set initial stack pointer
-    mov fp, #0          // init frame pointer
-    bl _cstart          // call _cstart
-hang: b hang        	// after _cstart returns, go into infinite loop
+    mov sp, #0x8000000
+    mov fp, #0
+    bl _cstart
+hang: b hang
