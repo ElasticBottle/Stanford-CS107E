3b4ce8884a773541b9b1bdae5881dccb534fb718
Julie Zelenski <zelenski@cs.stanford.edu>
2019-01-31 03:14:37 +0000

    Update public version of libpi header files commit e75ddbdee537c885f490532d619a466f8c6741bb Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Wed Jan 30 19:08:45 2019 -0800
        Update public version of libpi header files

--- cs107e/include/strings.h
+++ cs107e/include/strings.h
@@ -20,32 +20,42 @@ void *memset(void *s, int c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 
 /*
- * Find the length of a string `s`, returned as an `int`.
+ * Find the length of string `s`.
  *
  * Returns the number of characters that precede the null-terminator.
  */
-int strlen(const char *s);
+size_t strlen(const char *s);
 
 /*
  * Lexicographically compare the null-terminated strings `s1` and `s2`.
  *
  * Returns an integer value greater than, equal to, or less than 0, according
- * as the string `s1` is greater than, equal to, or less than the string `s2`.
- * Comparison is done as unsigned characters.
+ * to whether the string `s1` is greater than, equal to, or less than 
+ * the string `s2`. Comparison is done as unsigned characters.
  */
 int strcmp(const char *s1, const char *s2);
 
 /*
- * Convert the digit characters in `str` to corresponding unsigned integer
- * value. If `str` begins with the prefix "0x", the characters of `str`
- * will be interpreted as hexadecimal digits (base 16); otherwise characters
+ * Convert the digit characters in `str` to the corresponding unsigned integer
+ * value. If `str` begins with the prefix "0x", the characters of `str` will
+ * be interpreted as hexadecimal digits (base 16); otherwise the characters
  * are interpreted as decimal digits (base 10). No other bases are supported.
- * The conversion processes all characters of `str`, stopping early
- * if it encounters a character that is not a valid digit in base.
- * If `endptr` is not NULL, *endptr is assigned the address of the character
+ *
+ * `strtonum` processes the characters of `str`, stopping at the first
+ * character that is not a valid digit in the base or at the terminating
+ * null (whichever comes first).  The function is not required to support 
+ * leading spaces or a plus/minus sign. Such characters can be
+ * treated as invalid and stop the conversion.
+ *
+ * The argument `endptr` is an output parameter optionally used to  
+ * communicate back to the caller what remains in `str` after having 
+ * "consumed" the digit characters. A caller can pass NULL for `endptr`
+ * if they do not need this information.
+ *
+ * If `endptr` is not NULL, *endptr is updated to point to the character
  * in `str` where conversion stopped. This is either the address of the
  * first invalid character in `str` or the address of the terminating null
- * char in `str` if all characters are valid digits.
+ * if all characters in `str` are valid digits.
  *
  * Returns the result of the conversion as an unsigned integer. Returns 0
  * if the first character of `str` is not a valid digit.
@@ -55,10 +65,12 @@ unsigned int strtonum(const char *str, const char **endptr);
 /*
  * Size-bounded string concatenation. Append the null-terminated string `src`
  * to the end of `dst`. Appends at most `maxsize - strlen(dst) - 1` bytes, and
- * null-terminates the result.
+ * null-terminates `dst`.
  *
- * Returns the initial length of `dst` plus the length of `src`.
+ * Returns the initial length of `dst` plus the length of `src`. This is 
+ * equal to the number of characters it would have written to `dst` if there 
+ * were space.
  */
-int strlcat(char *dst, const char *src, int maxsize);
+size_t strlcat(char *dst, const char *src, size_t maxsize);
 
 #endif
