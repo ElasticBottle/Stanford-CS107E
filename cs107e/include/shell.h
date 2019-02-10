b92fe68c8f3d8b5fd9cdb6efea82b230bc75ccea
Julie Zelenski <zelenski@cs.stanford.edu>
2019-02-10 08:14:49 +0000

    Clarify shell requirements in interface commit 751458e0eae178ca84f21d6ebe3adf5a4f7986bd Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Sun Feb 10 00:09:48 2019 -0800
        Clarify shell requirements in interface

--- cs107e/include/shell.h
+++ cs107e/include/shell.h
@@ -6,16 +6,13 @@
  * of your shell in assignment 5 and complete it in assignment 7.
  *
  * Author: Julie Zelenski <zelenski@cs.stanford.edu>
- * Date: Februrary 2016
+ * Last update: February 2019
  */
 
 typedef int (*formatted_fn_t)(const char *format, ...);
 
 /*
- * shell_init
- * ==========
- *
- * Initialize the shell
+ * `shell_init`: Required initialization for shell
  *
  * Takes a *function pointer* `printf_fn` that is used
  * to configure where the shell directs its output.
@@ -30,12 +27,21 @@ typedef int (*formatted_fn_t)(const char *format, ...);
 void shell_init(formatted_fn_t print_fn);
 
 /*
- * shell_readline
- * ==============
+ * `shell_bell`: audio/visual beep
  *
- * Reads a single line of input from the user. Store the characters typed on
- * the keyboard and stores them into a buffer `buf` of size `bufsize`. Reading
- * stops when the user types enter (\n).
+ * Send Ascii BEL character '\a' over the uart. 
+ * https://en.wikipedia.org/wiki/Bell_character
+ */
+void shell_bell(void);
+
+/*
+ * `shell_readline`: read function of shell read-eval-print loop
+ *
+ * Reads a single line of input from the user. Reads the characters typed on
+ * the keyboard and stores them into `buf`. Reading stops when the user
+ * types Return ('\n') or when `buf` is full (`bufsize` - 1), whichever
+ * comes first. The contents written to `buf` are null-terminated. 
+ * The ending newline is discarded (not written to buf).
  *
  * When the user types backspace (\b):
  *   If there are any characters currently in the buffer, deletes the last one.
@@ -44,28 +50,35 @@ void shell_init(formatted_fn_t print_fn);
 void shell_readline(char buf[], int bufsize);
 
 /*
- * shell_evaluate
- * ==============
+ * `shell_evaluate`: eval function of shell read-eval-print loop
  *
  * Parses a line as follows:
- *   * If the line is empty, does nothing
- *   * Otherwise, tokenizes the line. A token is a sequence of non-space
- *     characters.
- *   * Looks up the command function associated with the command name (first token).
- *   * If the command does not exist, prints an error such as:
- *       error: no such command 'binky'.
- *   * Otherwise, executes the command function with the rest of the tokens,
- *     using the rest of the tokens as arguments
+ *   - If the line is empty/all space, do nothing
+ *   - Otherwise, tokenize the line. A token is a sequence of non-space
+ *     characters. There may be arbitrary whitespace in between tokens, 
+ *.    as well as leading and trailing whitespace. Whitespace includes
+ *     space, tab, and newline.
+ *   - The first token is the command name.
+ *   - Looks up the command function associated with the command name.
+ *   - If no such command is found, prints an error:
+ *           error: no such command 'binky'.
+ *   - Otherwise, executes the command function, passing the rest of 
+ *     the tokens as arguments.
+ *
+ * Returns the result of the call to the command function, or -1 if no 
+ * command was executed.
  */
 int shell_evaluate(const char *line);
 
 /* 
- * shell_run
- * =========
+ * `shell_run`: Top level shell interface.
  *
- * Main function of the shell module, must be called after `shell_init`.
- * Enters a read, eval, print loop that uses the  `shell_readline`, and
- * `shell_evaluate`, `printf_fn` (argument to `shell_init`).
+ * Main function of the shell module. Must be preceded by calls
+ * to `shell_init` and `keyboard_init`.
+ *
+ * Enters a read-eval-print loop built of out of `shell_readline`, 
+ * `shell_evaluate`, and the `print_fn` argument to `shell_init`.
+ * This function never returns.
  */
 void shell_run(void);
 
