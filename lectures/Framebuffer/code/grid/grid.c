ff4f29d8971714babe8c5466b2da68ce9de92287
Pat Hanrahan <hanrahan@cs.stanford.edu>
2019-02-15 16:10:56 +0000

    Release lecture commit 43295cfe605d5d61a252c5e19ad8507693b3eb83 Author: Pat Hanrahan <hanrahan@cs.stanford.edu> Date:   Fri Feb 15 08:03:33 2019 -0800
        Release lecture

---
+++ lectures/Framebuffer/code/grid/grid.c
@@ -0,0 +1,54 @@
+#include "fb.h"
+#include "gl.h"
+
+const unsigned WIDTH = 1280;
+const unsigned HEIGHT = 1024;
+const unsigned DEPTH = 4;
+
+// 2D
+void draw_pixel(int x, int y, color_t c)
+{
+    //unsigned int width = fb_get_pitch()/4;
+    //unsigned int (*im)[width] = (unsigned int (*)[WIDTH])fb_get_draw_buffer();
+
+    unsigned int (*im)[WIDTH] = (unsigned int (*)[WIDTH])fb_get_draw_buffer();
+
+    im[y][x] = c;
+}
+
+/*
+// 1D
+void draw_pixel(int x, int y, color_t c)
+{
+    //unsigned int width = fb_get_pitch()/4;
+    //*(im + y * width + x) = c; 
+
+    unsigned int *im = (unsigned int *)fb_get_draw_buffer();
+    *(im + y * WIDTH + x) = c; 
+}
+*/
+
+void draw_hline(int y, color_t c)
+{
+    for( int x = 0; x < fb_get_width(); x++ )
+        draw_pixel(x, y, c);
+}
+
+void draw_vline(int x, color_t c)
+{
+    for( int y = 0; y < fb_get_height(); y++ )
+        draw_pixel(x, y, c);
+}
+
+
+void main(void) 
+{
+  fb_init(WIDTH, HEIGHT, DEPTH, FB_SINGLEBUFFER);
+
+  for( int y = 0; y < HEIGHT; y += 16 )
+      draw_hline(y, GL_WHITE);
+  for( int x = 0; x < WIDTH; x += 16 )
+      draw_vline(x, GL_WHITE);
+
+}
+
